5d03dca8dcb529ff43b2fee88526bf974319a9de
Mike West <mike@mikewest.org>
2009-02-22 11:14:44 +0100

    Adding install script.  Any reason _not_ to just symlink everything in?

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+ln -s ~/Repositories/homedir/etc/ ~/etc
+ln -s ~/Repositories/homedir/bin/ ~/bin
+ln -s ~/Repositories/homedir/lib/ ~/lib
+ln -s ~/Repositories/homedir/.bashrc ~/.bashrc
+ln -s ~/Repositories/homedir/.bash_profile  ~/.bash_profile
+ln -s ~/Repositories/homedir/.inputrc ~/.inputrc
+ln -s ~/Repositories/homedir/.screenrc ~/.screenrc
+ln -s ~/Repositories/homedir/.gitignore ~/.gitignore
+ln -s ~/Repositories/homedir/.gitconfig ~/.gitconfig
